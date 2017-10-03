ddca6fafe29629b7212c5a77946cf69ee8820114
azad <azad_ce@yahoo.com>
2017-10-03 16:43:35 +0200

    initial project

---
+++ mvcRMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcRMS.Startup))]
+namespace mvcRMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
